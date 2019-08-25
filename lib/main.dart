ed216e3b7bff10fba9a6fb9db6026ff50e361c9a
Cédric <cedric.creusot@gmail.com>
2019-08-25 22:42:56 +0200

    Add network call to the sample apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -26,12 +30,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  String _searchLocation = "";
+  bool _searchPending = false;
+  Weather _currentWeather = null;
 
-  void requestWeatherFor(String location) {
-    Scaffold.of(context).showSnackBar(SnackBar(
-      content: Text("test showing location: $location"),
-    ));
+  void requestWeatherFor(String location) async {
+    var response = await http.get("https://www.prevision-meteo.ch/services/json/$location");
+    var bodyResponse = json.decode(response.body);
+
+    setState(() {
+      _searchPending = false;
+      _currentWeather = Weather(bodyResponse['city_info']['name'],
+        bodyResponse['current_condition']['tmp'],
+        bodyResponse['current_condition']['icon']
+      );
+    });
   }
 
   @override
@@ -43,23 +55,49 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: <Widget>[
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(hintText: "Enter your city"),
-                  textInputAction: TextInputAction.search,
-                  onChanged: (String value) => _searchLocation = value,
-                  onEditingComplete: () {
-                    requestWeatherFor(_searchLocation);
-                  },
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(hintText: "Enter your city"),
+                      textInputAction: TextInputAction.search,
+                      onSubmitted: (value) {
+                        requestWeatherFor(value);
+                        setState(() {
+                          _searchPending = true;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    if (_searchPending) Center(child: CircularProgressIndicator())
+                    else if (_currentWeather != null) Center(child:
+                      Image.network(_currentWeather.icon),)
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+
+class Weather {
+
+  final String name;
+  final int currentTemperature;
+  final String icon;
+
+  Weather(this.name, this.currentTemperature, this.icon);
 }
